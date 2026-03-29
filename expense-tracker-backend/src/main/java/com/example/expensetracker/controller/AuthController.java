b9a35050107648c3bb97a329f1566e4f732134bb
TAkshay-Tej <akshaytej75@gmail.com>
2026-03-29 22:33:55 +0530

    Added backend

---
+++ expense-tracker-backend/src/main/java/com/example/expensetracker/controller/AuthController.java
@@ -0,0 +1,33 @@
+package com.example.expensetracker.controller;
+
+
+import com.example.expensetracker.dto.AuthDto;
+import com.example.expensetracker.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthService authService;
+
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthDto.AuthResponse> register(
+            @Valid @RequestBody AuthDto.RegisterRequest request) {
+        return ResponseEntity.ok(authService.register(request));
+    }
+
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthDto.AuthResponse> login(
+            @Valid @RequestBody AuthDto.LoginRequest request) {
+        return ResponseEntity.ok(authService.login(request));
+    }
+}
